e55372623c846040f7c8f477c7ae7b463eef0830
Dongsung Kim <kiding@me.com>
2019-07-06 14:21:07 +0900

    Consistency.

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,12 +5,13 @@ import re
 from datetime import datetime
 
 def load_new_song_data(local_music_json_path, server_music_json_path):
-    with open(local_music_json_path, 'r', encoding='utf-8') as local_music_data:
-        local_music_map = _json_to_id_value_map(json.load(local_music_data))
+    with open(local_music_json_path, 'r', encoding='utf-8') as f:
+        local_music_data = json.load(f)
+        local_music_map = _json_to_id_value_map(local_music_data)
 
-    # load server music.json and update local music.json file if any song data appended
     server_music_data = requests.get(server_music_json_path).json()
     server_music_map = _json_to_id_value_map(server_music_data)
+    
     if len(server_music_map) > len(local_music_map):
         with open(local_music_json_path, 'w', encoding='utf-8') as f:
             json.dump(server_music_data, f, ensure_ascii=False, indent=2)
@@ -27,16 +28,16 @@ def renew_music_ex_data(new_song_list, local_music_ex_json_path, server_music_ja
         print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " nothing updated")
         return
 
-    with open(local_music_ex_json_path, 'r', encoding='utf-8') as local_music_ex_data:
-        music_ex_data = json.load(local_music_ex_data)
+    with open(local_music_ex_json_path, 'r', encoding='utf-8') as f:
+        local_music_ex_data = json.load(f)
 
     for song in new_song_list:
         _download_song_jacket(song, server_music_jacket_base_url)
-        _add_song_data_to_ex_data(song, music_ex_data)
+        _add_song_data_to_ex_data(song, local_music_ex_data)
         print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " new song data downloaded : " + song['title'])
 
     with open(local_music_ex_json_path, 'w', encoding='utf-8') as f:
-        json.dump(music_ex_data, f, ensure_ascii=False, indent=2)
+        json.dump(local_music_ex_data, f, ensure_ascii=False, indent=2)
 
 
 def _download_song_jacket(song, server_music_jacket_base_url):
@@ -57,14 +58,12 @@ def _add_song_new_data_name(song):
     song['lev_lnt_i'] = ""
     return song
 
-def renew_lastupdated(local_index_html_path):
-    with open(local_index_html_path, 'r', encoding='utf-8') as f:
-        html = f.read()
-        f.close()
+def renew_lastupdated(local_html_path):
+    with open(local_html_path, 'r', encoding='utf-8') as f:
+        local_html_data = f.read()
 
     value = f'<span>DATE: {datetime.now().strftime("%Y%m%d")}</span>'
-    html = re.sub(r'(<[^>]+ class="lastupdated"[^>]*>).*(</[^>]+>)', rf'\1{value}\2', html, flags=re.IGNORECASE)
+    local_html_data = re.sub(r'(<[^>]+ class="lastupdated"[^>]*>).*(</[^>]+>)', rf'\1{value}\2', local_html_data, flags=re.IGNORECASE)
 
-    with open(local_index_html_path, 'w', encoding='utf-8') as f:
-        f.write(html)
-        f.close()
+    with open(local_html_path, 'w', encoding='utf-8') as f:
+        f.write(local_html_data)
