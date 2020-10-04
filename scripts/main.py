6eaaa30bb0cb6696dddc14379698ac98ac89b87b
Hansol Kim <kimatg@gmail.com>
2020-10-04 19:21:41 +0900

    Update song data 20201001

--- scripts/main.py
+++ scripts/main.py
@@ -5,7 +5,7 @@ server_music_jacket_base_url = "https://ongeki.sega.jp/assets/img/pages/music/th
 local_music_json_path = "data/music.json"
 local_music_ex_json_path = "data/music-ex.json"
 local_index_html_path = "index.html"
-local_levels_html_path = "levels.html"
+local_levels_html_path = "lv/index.html"
 local_namuwiki_export_path = "namuwiki-export.html"
 
 new_song_data = load_new_song_data(local_music_json_path, server_music_data_url)
