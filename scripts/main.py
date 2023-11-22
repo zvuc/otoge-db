60d6c011b54d9615ddabf29e6891b88856566fa5
Hansol Kim <kimatg@gmail.com>
2023-11-22 15:39:35 +0900

    Add skipwiki arg to skip wiki fetch

--- scripts/main.py
+++ scripts/main.py
@@ -12,11 +12,12 @@ local_namuwiki_export_path = "namuwiki-export.html"
 
 parser = argparse.ArgumentParser(description='Description of your script')
 parser.add_argument('--nocolors', action="store_true", help='Print messages in color')
+parser.add_argument('--skipwiki', action="store_true", help='Skip wiki fetch')
 
 args = parser.parse_args()
 
 new_song_data = load_new_song_data(local_music_json_path, server_music_data_url)
-renew_music_ex_data(new_song_data, local_music_ex_json_path, server_music_jacket_base_url, local_diffs_log_path, args.nocolors)
+renew_music_ex_data(new_song_data, local_music_ex_json_path, server_music_jacket_base_url, local_diffs_log_path, args.nocolors, args.skipwiki)
 renew_lastupdated(local_music_ex_json_path, local_index_html_path)
 renew_lastupdated(local_music_ex_json_path, local_levels_html_path)
 renew_lastupdated(local_music_ex_json_path, local_namuwiki_export_path)
