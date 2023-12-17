5d47b21f4f8a1ecb94d25402c954b1403ffabdc4
Hansol Kim <kimatg@gmail.com>
2023-12-17 23:36:26 +0900

    Refactor wiki script, reformat message prints

--- scripts/wikiwiki.py
+++ scripts/wikiwiki.py
@@ -66,6 +66,8 @@ def _filter_songs_by_id(song_list, song_id):
 
 
 def _update_song_wiki_data(song, nocolors, escape):
+    _print_message(f"{song['id']} {song['title']}", nocolors, bcolors.ENDC, escape)
+
     title = (
         song['title']
         .replace('&', '＆')
@@ -80,8 +82,6 @@ def _update_song_wiki_data(song, nocolors, escape):
     if 'wikiwiki_url' in song and song['wikiwiki_url']:
         url = song['wikiwiki_url']
         wiki = requests.get(url)
-
-        _print_message("(URL already present!)", song, nocolors, bcolors.OKBLUE, escape)
         return _parse_wikiwiki(song, wiki, url, nocolors, escape)
 
     # If not, guess URL from title
@@ -97,26 +97,28 @@ def _update_song_wiki_data(song, nocolors, escape):
 
             if not wiki.ok:
                 # give up
-                _print_message("failed to guess wiki page", song, nocolors, bcolors.FAIL, escape)
+                _print_message("Failed to guess wiki page", nocolors, bcolors.FAIL, escape)
                 return song
 
             else:
                 url = guess_url
+                _print_message("Found URL by guess!", nocolors, bcolors.OKBLUE, escape)
                 return _parse_wikiwiki(song, wiki, url, nocolors, escape)
                 
         else:
             url = guess_url
-            
+            _print_message("Found URL by guess!", nocolors, bcolors.OKBLUE, escape)
             return _parse_wikiwiki(song, wiki, url, nocolors, escape)
 
 
 def _parse_wikiwiki(song, wiki, url, nocolors, escape):
     soup = BeautifulSoup(wiki.text, 'html.parser')
     tables = soup.select("#body table")
+    old_song = song
 
     # If there are no tables in page at all, exit
     if len(tables) == 0:
-        _print_message("Parse failed! Skipping song", song, nocolors, bcolors.FAIL, escape)
+        _print_message("Parse failed! Skipping song", nocolors, bcolors.FAIL, escape)
         return song
 
     # find the overview table
@@ -142,30 +144,40 @@ def _parse_wikiwiki(song, wiki, url, nocolors, escape):
 
         overview_heads = [head.text for head in overview_heads]
         overview_data = [data[0].text for data in overview_data]
-        overview_hash = dict(zip(overview_heads, overview_data))
+        overview_dict = dict(zip(overview_heads, overview_data))
 
         # Find enemy lv data
-        if 'LV.' in overview_hash["対戦相手"].upper():
-            enemy_info = overview_hash["対戦相手"].upper().split("LV.")
+        if 'LV.' in overview_dict["対戦相手"].upper():
+            enemy_info = overview_dict["対戦相手"].upper().split("LV.")
             enemy_name = enemy_info[0]
             enemy_lv = enemy_info[1]
-            song['enemy_lv'] = enemy_lv
+
+            if enemy_lv and not enemy_lv == '○':
+                diff_count = [0]
+                _update_song_key(song, 'enemy_lv', enemy_lv, diff_count=diff_count)
+
+                if diff_count[0] > 0:
+                    _print_message("Added Enemy Lv", nocolors, bcolors.OKGREEN, escape)
+
 
             # If character name includes type info, use it
             if 'FIRE' in enemy_name:
                 song['enemy_type'] = 'FIRE'
+                _print_message("Updated enemy type", nocolors, bcolors.OKGREEN, escape)
             elif 'AQUA' in enemy_name:
                 song['enemy_type'] = 'AQUA'
+                _print_message("Updated enemy type", nocolors, bcolors.OKGREEN, escape)
             elif 'LEAF' in enemy_name:
                 song['enemy_type'] = 'LEAF'
+                _print_message("Updated enemy type", nocolors, bcolors.OKGREEN, escape)
 
         else:
             # fail
-            _print_message("Warning - enemy lv not found", song, nocolors, bcolors.WARNING, escape)
+            _print_message("Warning - enemy lv not found", nocolors, bcolors.WARNING, escape)
             
     else:
         # fail
-        _print_message("Warning - overview table not found", song, nocolors, bcolors.WARNING, escape)
+        _print_message("Warning - overview table not found", nocolors, bcolors.WARNING, escape)
 
 
     # find the charts table
@@ -176,50 +188,44 @@ def _parse_wikiwiki(song, wiki, url, nocolors, escape):
             charts_table = table
             break
     
+    # Update chart details
     if charts_table:
         charts_table_head = [th.text for th in charts_table.select("thead th")]
-        charts_data = [[cell.text for cell in level.select("th,td")] for level in charts_table.select("tbody tr")]
+        charts_data = [[cell.text for cell in chart.select("th,td")] for chart in charts_table.select("tbody tr")]
 
         if any(charts_table_head) and '難易度' in charts_table_head[0]:
             for chart_details in charts_data:
-                level_hash = dict(zip(charts_table_head, chart_details))
+                chart_dict = dict(zip(charts_table_head, chart_details))
 
-                if song['lunatic'] == '' and level_hash['難易度'] == 'BASIC':
-                    _update_song_key(song["lev_bas_notes"], level_hash["総ノート数"], remove_comma=True)
-                    _update_song_key(song["lev_bas_bells"], level_hash["BELL"], remove_comma=True)
-                    _update_song_key(song["lev_bas_i"], level_hash["譜面定数"])
-                    _update_song_key(song["lev_bas_designer"], level_hash["譜面製作者"])
-                elif song['lunatic'] == '' and level_hash['難易度'] == 'ADVANCED':
-                    _update_song_key(song["lev_adv_notes"], level_hash["総ノート数"], remove_comma=True)
-                    _update_song_key(song["lev_adv_bells"], level_hash["BELL"], remove_comma=True)
-                    _update_song_key(song["lev_adv_i"], level_hash["譜面定数"])
-                    _update_song_key(song["lev_adv_designer"], level_hash["譜面製作者"])
-                elif song['lunatic'] == '' and level_hash['難易度'] == 'EXPERT':
-                    _update_song_key(song["lev_exc_notes"], level_hash["総ノート数"], remove_comma=True)
-                    _update_song_key(song["lev_exc_bells"], level_hash["BELL"], remove_comma=True)
-                    _update_song_key(song["lev_exc_i"], level_hash["譜面定数"])
-                    _update_song_key(song["lev_exc_designer"], level_hash["譜面製作者"])
-                elif song['lunatic'] == '' and level_hash['難易度'] == 'MASTER':
-                    _update_song_key(song["lev_mas_notes"], level_hash["総ノート数"], remove_comma=True)
-                    _update_song_key(song["lev_mas_bells"], level_hash["BELL"], remove_comma=True)
-                    _update_song_key(song["lev_mas_i"], level_hash["譜面定数"])
-                    _update_song_key(song["lev_mas_designer"], level_hash["譜面製作者"])
-                elif song['lunatic'] == '1' and level_hash['難易度'] == 'LUNATIC':
-                    _update_song_key(song["lev_lnt_notes"], level_hash["総ノート数"], remove_comma=True)
-                    _update_song_key(song["lev_lnt_bells"], level_hash["BELL"], remove_comma=True)
-                    _update_song_key(song["lev_lnt_i"], level_hash["譜面定数"])
-                    _update_song_key(song["lev_lnt_designer"], level_hash["譜面製作者"])
+                if song['lunatic'] == '' and chart_dict['難易度'] == 'BASIC':
+                    _update_song_chart_details(song, chart_dict, 'bas', nocolors, escape)
+                elif song['lunatic'] == '' and chart_dict['難易度'] == 'ADVANCED':
+                    _update_song_chart_details(song, chart_dict, 'adv', nocolors, escape)
+                elif song['lunatic'] == '' and chart_dict['難易度'] == 'EXPERT':
+                    _update_song_chart_details(song, chart_dict, 'exc', nocolors, escape)
+                elif song['lunatic'] == '' and chart_dict['難易度'] == 'MASTER':
+                    _update_song_chart_details(song, chart_dict, 'mas', nocolors, escape)
+                elif song['lunatic'] == '1' and chart_dict['難易度'] == 'LUNATIC':
+                    _update_song_chart_details(song, chart_dict, 'lnt', nocolors, escape)
         else:
-            _print_message("Warning - No chart table found", song, nocolors, bcolors.WARNING, escape)
+            _print_message("Warning - No chart table found", nocolors, bcolors.WARNING, escape)
     else:
-        _print_message("Warning - No chart table found", song, nocolors, bcolors.WARNING, escape)
+        _print_message("Warning - No chart table found", nocolors, bcolors.WARNING, escape)
     
+    # Update BPM
+    if overview_dict['BPM']:
+        diff_count = [0]
+        _update_song_key(song, 'bpm', overview_dict['BPM'], diff_count=diff_count)
 
-    song['bpm'] = overview_hash['BPM']
+        if diff_count[0] > 0:
+            _print_message("Added BPM", nocolors, bcolors.OKGREEN, escape)
 
     song['wikiwiki_url'] = url
 
-    _print_message("Updated song extra data from wiki", song, nocolors, bcolors.OKGREEN, escape)
+    if old_song == song:
+        _print_message("Done (Nothing updated)", nocolors, bcolors.ENDC, escape)
+    # else:
+    #     _print_message("Updated song extra data from wiki", nocolors, bcolors.OKGREEN, escape)
 
     return song
 
@@ -234,39 +240,40 @@ def get_last_date(LOCAL_MUSIC_JSON_PATH):
     
     return lastupdated
 
-def _update_song_key(key, new_data, remove_comma=False):
+def _update_song_chart_details(song, chart_dict, chart, nocolors, escape):
+    diff_count = [0]
+    _update_song_key(song, f"lev_{chart}_notes", chart_dict["総ノート数"], remove_comma=True, diff_count=diff_count)
+    _update_song_key(song, f"lev_{chart}_bells", chart_dict["BELL"], remove_comma=True, diff_count=diff_count)
+    _update_song_key(song, f"lev_{chart}_i", chart_dict["譜面定数"], diff_count=diff_count)
+    _update_song_key(song, f"lev_{chart}_designer", chart_dict["譜面製作者"], diff_count=diff_count)
+
+    if diff_count[0] > 0:
+        _print_message(f"Updated chart details for {chart.upper()}", nocolors, bcolors.OKGREEN, escape)
+
+def _update_song_key(song, key, new_data, remove_comma=False, diff_count=None):
     # if source is not empty, don't overwrite
-    if not (key == ''):
+    if not (song[key] == ''):
         return
-    # Only overwrite if new data is not empty
-    if not (new_data == ''):
-        key = new_data
+    # Only overwrite if new data is not empty and is not same
+    if not (new_data == '') and not (song[key] == new_data):
+        diff_count[0] += 1
+        song[key] = new_data
 
         if remove_comma:
-            key.replace(',', '')
-            return
-        else:
-            return
+            song[key] = song[key].replace(',', '')
+        
+        return
 
-def _print_message(message, song, nocolors, color_name, escape):
+def _print_message(message, nocolors, color_name, escape):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     reset_color = bcolors.ENDC
 
-    if song:
-        song_id = song['id']
-
-        # if --escape is set
-        if escape:
-            song_title = ' : ' + song['title'].replace("'", r"\'")
-        else:
-            song_title = ' : ' + song['title']
-    else:
-        song_id = ''
-        song_title = ''
+    if escape:
+        message = message.replace("'", r"\'")
 
     # if --nocolors is set
     if nocolors:
         color_name = ''
         reset_color = ''
 
-    print(timestamp + color_name + ' ' + song_id + ' ' + message + song_title + reset_color)
+    print(timestamp + ' ' + color_name + message + reset_color)
