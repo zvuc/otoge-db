e99eaca321328946f5079c6bbd2a63b6029a38ec
Hansol Kim <kimatg@gmail.com>
2024-01-05 01:33:57 +0900

    maimai wip

--- maimai/src/js/maimai.table-config.js
+++ maimai/src/js/maimai.table-config.js
@@ -360,9 +360,9 @@ var columns_params = [
 var default_order = 
     flat_view ?
         // 難易度 , Lv , Date
-        [[19, 'desc'],[18, 'desc'],[28, 'desc']] :
+        [[getColumnIndexByName('chart_lev'), 'desc'],[getColumnIndexByName('chart_diff'), 'desc'],[getColumnIndexByName('date'), 'desc']] :
         // date , ID
-        [[28, 'desc'],[0, 'asc']];
+        [[getColumnIndexByName('date'), 'desc'],[getColumnIndexByName('id'), 'asc']];
 
 function convertWEStars(we_star) {
     const conversionTable = {
@@ -649,162 +649,9 @@ $(document).ready(function() {
                 }) : null
             },
             "scrollX": true,
-
-            initComplete: function () {
-                var rows = this.api().rows().data();
-
-                // Generate Filter dropdown per columns
-                var table = this.api();
-                table.columns().every(function () {
-                    var order = table.order();
-                    var column = this;
-                    var column_data = column.data();
-                    var column_param = columns_params[column.index()];
-
-                    if (("filterable" in column_param) && (column_param.filterable == true)) {
-                        var selectWrap = $('<div class="select-wrap ' + column_param.className + '"><span class="label">' + column_param.displayTitle + '</span></div>')
-                            .appendTo($('.toolbar.filters'));
-                        var select = $('<select id="' + column_param.name + '"><option value="" data-default>——</option></select>');
-
-                        select.appendTo(selectWrap);
-
-                        select.on('change', function () {
-                            var val = $(this).val();
-                            var val_e = $.fn.dataTable.util.escapeRegex(
-                                $(this).val()
-                            );
-
-                            appendSelectboxStateClass($(this), val);
-
-                            // var val = $(this).val();
-
-                            // when applying filter, control rowgroup visibility
-                            if (column.index() === 28 || (val_e === "" && order[0][0] === 28)) {
-                                column.rowGroup().enable();
-                            } else {
-                                column.rowGroup().disable();
-                                // console.log('group disabled (filter active)');
-                            }
-
-                            // update URL params on change
-                            updateQueryStringParameter(column_param.name, val);
-
-                            column
-                                .search(val_e ? '^' + val_e + '$' : '', true, false)
-                                .draw();
-
-
-                        });
-
-
-                        // column parameter has customDropdownSortSource option
-                        if (column_param.customDropdownSortSource) {
-                            column_data = column_data.map(function (_, index) {
-                                // get index of column
-                                return index;
-                            }).sort(function (index_a, index_b) {
-                                // get index_a-th row and index_b-th row
-                                var row_a = rows[index_a], row_b = rows[index_b];
-                                // if customDropdownSortSource option is function type use it as comparator
-                                if (typeof column_param.customDropdownSortSource === 'function') {
-                                    return column_param.customDropdownSortSource(row_a, row_b);
-                                    // if customDropdownSortSource option is string type use it as comparator column key
-                                } else {
-                                    return row_a[column_param.customDropdownSortSource].localeCompare(row_b[column_param.customDropdownSortSource])
-                                }
-                                // get column data again since we converted this array to row array in previous lines
-                            }).map(function (index) {
-                                // get index-th column's data
-                                return column_data[index];
-                            });
-                        } else {
-                            column_data = column_data.sort();
-                        }
-
-                        // reverse sort for date
-                        if (column_param.reverseSortOrder) {
-                            column_data.reverse();
-                        }
-
-                        // draw option list
-                        column_data.unique().each(function (d, j) {
-                            if (d != '') {
-                                select.append('<option value="' + d + '">' + d + '</option>');
-                            }
-                        });
-
-                        // set value for select on page init
-                        if ('URLSearchParams' in window) {
-                            var searchParamValue = searchParams.get(column_param.name);
-                            if ( searchParamValue !== null ) {
-                                var value = unescapeSlashes(searchParamValue)
-                                column_data.unique().each(function (d) {
-                                    select.val(value);
-                                });
-                                appendSelectboxStateClass(select, value);
-                            }
-                        }
-
-                    }
-
-                });
-
-                // filter according to URL params on initial load
-                if ('URLSearchParams' in window) {
-                    searchParams.forEach(function (value, key) {
-                        table.columns().every(function () {
-                            var column = this;
-                            var column_param = columns_params[column.index()];
-                            var searchParamValue = searchParams.get(column_param.name);
-                            var searchParamValue_e = $.fn.dataTable.util.escapeRegex(
-                                decodeURIComponent(searchParamValue)
-                            );
-
-                            if ( searchParamValue !== null ) {
-                                column.search(searchParamValue ? '^' + searchParamValue_e + '$' : '', true, false);
-                            }
-                        });
-                    });
-
-                    table.draw();
-                }
-
-                table.on('order.dt', function () {
-                    var order = table.order();
-                    var search = table.columns().search();
-                    var searchActive = false;
-
-                    for (let k = 0; k < search.length; k = k + 1) {
-                        if (k in search && search[k] !== "") {
-                            searchActive = true;
-                            break;
-                        }
-                    }
-
-                    // Disable rowgroup unless sorting by date
-                    if (order[0][0] !== 27) {
-                        table.rowGroup().disable();
-                        // console.log('group disabled (sorting by non-date column)');
-                        return;
-                    }
-                    // enable rowgroup if sorting by date AND search is inactive
-                    else if ((order[0][0] === 27) && !searchActive) {
-                        table.rowGroup().enable();
-                        // console.log('group enabled (sorting by date + search inactive)');
-                        return;
-                    }
-                    // do nothing
-                    else {
-                        // console.log('do nothing');
-                        return;
-                    }
-                    table.draw();
-                });
-
-                $('#table').addClass('loading-done');
-                $('html').removeClass('table-loading');
-
-                // $('.column-toggle-bar').prepend('<span class="label">カラムON/OFF</span>');
+            initComplete: function() {
+                var table = this;
+                tableInitCompleteFunctions(table);
             }
         });
     });
