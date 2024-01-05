f62765d5b74afde968ee4482ffdb3996f00b90ca
Hansol Kim <kimatg@gmail.com>
2024-01-06 02:05:33 +0900

    maimai wip (add args to script)

--- scripts/update-chartguide-data.py
+++ scripts/update-chartguide-data.py
@@ -4,6 +4,7 @@ import shared
 parser = argparse.ArgumentParser(description='Description of your script')
 parser.add_argument('--ongeki', action="store_true", help='Perform scripts for ongeki')
 parser.add_argument('--chunithm', action="store_true", help='Perform scripts for chunithm')
+parser.add_argument('--maimai', action="store_true", help='Perform scripts for maimai')
 parser.add_argument('--date_from', type=int, default=0, help='Date range from')
 parser.add_argument('--date_until', type=int, default=0, help='Date range until')
 parser.add_argument('--id', type=int, default=0, help='Song ID')
@@ -17,8 +18,10 @@ if args.ongeki:
 	import ongeki as game_module
 elif args.chunithm:
 	import chunithm as game_module
-elif not args.ongeki and not args.chunithm:
-	print('Please specify which game: --ongeki, --chunithm')
+elif args.maimai:
+	import maimai as game_module
+elif not args.ongeki and not args.chunithm and not args.maimai:
+	print('Please specify which game: --ongeki, --chunithm, --maimai')
 	exit()
 
 game_module.chartguide.update_chartguide_data(args)
