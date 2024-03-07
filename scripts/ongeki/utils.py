db8be1f043447dabdfef639813f3033eace65392
Hansol Kim <kimatg@gmail.com>
2024-03-07 17:37:11 +0900

    feat(script/ongeki): push ongeki version to bright MEMORY Act.3

--- scripts/ongeki/utils.py
+++ scripts/ongeki/utils.py
@@ -238,7 +238,7 @@ def _add_ex_data_template(song):
     song['lev_lnt_bells'] = ""
     song['lev_lnt_designer'] = ""
     song['lev_lnt_chart_link'] = ""
-    song['version'] = "bright MEMORY"
+    song['version'] = "bright MEMORY Act.3"
     song['wikiwiki_url'] = ""
 
     return song
