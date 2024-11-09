0d8d8e2828299534fd6a80d3b56463d020b0d94d
Hansol Kim <kimatg@gmail.com>
2024-11-09 14:51:46 +0900

    feat(script/maimai): support parsing from legacy sgimera data format

--- scripts/maimai/const.py
+++ scripts/maimai/const.py
@@ -3,14 +3,16 @@ import requests
 import json
 import os
 import re
+import ast
 from bs4 import BeautifulSoup
 from shared.common_func import *
 from maimai.paths import *
 from datetime import datetime
 from math import log2
 
-# SGIMERA_URL = 'https://sgimera.github.io/mai_RatingAnalyzer/scripts_maimai/maidx_in_lv_data_prism.js'
 SGIMERA_URL = 'https://sgimera.github.io/mai_RatingAnalyzer/scripts_maimai/maidx_in_lv_prism_.js'
+# SGIMERA_URL = 'https://gist.githubusercontent.com/myjian/f059331eb9daefeb0dc57ce15e6f73e9/raw/'
+
 
 # Update on top of existing music-ex
 def update_const_data():
@@ -53,42 +55,52 @@ def fetch_js_data(url):
 def parse_sgimera_data(js_content):
     sgimera_data = {}
 
-    # Regex to find the main dictionary data within the file
-    in_lv_data = re.search(r"var in_lv = (\[.*?\]);", js_content, re.DOTALL)
-    if not in_lv_data:
-        return {}
+    # Parse for legacy format (no decoding needed)
+    if game.ARGS.legacy:
+        entries = ast.literal_eval(js_content)
 
-    entries = re.finditer(
-        r"\{dx:(\d+), v:(?:\s|)(\d+), lv:\[(.*?)\], n:`(.*?)`(?:, nn:`(.*?)`|), ico:`(.*?)`(?:, olv:(.*?)|)\}",
-        in_lv_data.group(1)
-    )
+        for entry in entries:
+            if len(entry['lv']) == 5 and entry['lv'][4] == '0':
+                entry['lv'].pop(4)
 
-    for entry in entries:
-        dx, version, levels, name, nn, ico, olv = entry.groups()
+            sgimera_data[entry['name']] = {
+                "dx": int(entry['dx']),
+                "v": int(entry['debut']),
+                "lv": entry['lv'],
+                "title": entry['name'],
+                "ico": entry['ico'] if 'ico' in entry else None
+            }
+    else:
+        # Regex to find the main dictionary data within the file
+        in_lv_data = re.search(r"var in_lv = (\[.*?\]);", js_content, re.DOTALL)
+        if not in_lv_data:
+            return {}
 
-        levels = levels.split(", ")
+        entries = re.finditer(
+            r"\{dx:(\d+), v:(?:\s|)(\d+), lv:\[(.*?)\], n:`(.*?)`(?:, nn:`(.*?)`|), ico:`(.*?)`(?:, olv:(.*?)|)\}",
+            in_lv_data.group(1)
+        )
 
-        if len(levels) > 5 and levels[4] == '0':
-            levels[4] = levels[5]
-            levels.pop(5)
+        for entry in entries:
+            dx, version, levels, name, nn, ico, olv = entry.groups()
 
-        sgimera_data[name] = {
-            "dx": int(dx),
-            "v": int(version),
-            "lv": [int(log2(x)) if x > 0 and log2(x).is_integer() else -1 for x in map(int, levels)],
-            "title": name,
-            "ico": ico if ico else None
-        }
+            levels = levels.split(", ")
 
+            if len(levels) > 5 and levels[4] == '0':
+                levels[4] = levels[5]
+                levels.pop(5)
+
+            sgimera_data[name] = {
+                "dx": int(dx),
+                "v": int(version),
+                "lv": [int(log2(x)) if x > 0 and log2(x).is_integer() else -1 for x in map(int, levels)],
+                "title": name,
+                "ico": ico if ico else None
+            }
 
     return sgimera_data
 
-chart_suffixes = {
-    1: "lev_adv",
-    2: "lev_exp",
-    3: "lev_mas",
-    4: "lev_remas"
-}
+
 def update_song_with_sgimera_data(song, sgimera_data, total_diffs):
     header_printed = [0]
 
@@ -101,10 +113,18 @@ def update_song_with_sgimera_data(song, sgimera_data, total_diffs):
     # Format song image URL for comparison
     song_icon = os.path.splitext(song['image_url'])[0]
 
+    # if song['title'] == 'クレイジークレイジーダンサーズ':
+    #     ipdb.set_trace()
+
     # Find matching entry in sgimera_dict by title and icon
     target_entry = None
     for title, entry in sgimera_data.items():
         if entry['ico'] == song_icon:
+            target_entry = entry
+            break
+        elif entry['title'] == song['title']:
+            lazy_print_song_header(f"{song['sort']}, {song['title']}, {song['version']}", header_printed, log=True)
+            print_message(f"Warning: matched song with just title", bcolors.WARNING, log=True)
             target_entry = entry
             break
 
@@ -112,24 +132,37 @@ def update_song_with_sgimera_data(song, sgimera_data, total_diffs):
         return False  # No match found
 
     for idx, decimal_part in enumerate(target_entry['lv']):
-        if decimal_part == -1 or idx == 0: # Skip Basic
+        if decimal_part <= -1 or idx == 0: # Skip Basic
             continue
 
-        chart_key = chart_suffixes[idx]
+        chart_key = game.CHART_LIST[idx]
 
         if target_entry['dx'] == 1:
             chart_key = f"dx_{chart_key}"
 
-        base_level = float(song[chart_key].replace("+", ".6"))
-        chart_const = (base_level * 10 + decimal_part) / 10
+        # Skip if chart doesn't exist
+        if chart_key not in song:
+            continue
+
+        if game.ARGS.legacy:
+            chart_const = decimal_part
+        else:
+            base_level = float(song[chart_key].replace("+", ".6"))
+            chart_const = (base_level * 10 + decimal_part) / 10
 
         chart_key += "_i"
 
         if chart_const is not None:
-            # ipdb.set_trace()
             if chart_key in song and song[chart_key] == str(chart_const):
-                lazy_print_song_header(f"{song['sort']}, {song['title']}, {song['version']}", header_printed, log=True, is_verbose=True)
-                print_message(f"No change ({chart_key}: {chart_const})", bcolors.ENDC, log=True, is_verbose=True)
+                if game.ARGS.overwrite:
+                    total_diffs[0] += 1
+                    lazy_print_song_header(f"{song['sort']}, {song['title']}, {song['version']}", header_printed, log=True)
+                    print_message(f"Overwrote chart constant ({chart_key}: {song[chart_key]} → {chart_const})", bcolors.WARNING, log=True)
+
+                    song[chart_key] = str(chart_const)  # Update song with the sgimera level constant
+                else:
+                    lazy_print_song_header(f"{song['sort']}, {song['title']}, {song['version']}", header_printed, log=True, is_verbose=True)
+                    print_message(f"No change ({chart_key}: {chart_const})", bcolors.ENDC, log=True, is_verbose=True)
             else:
                 total_diffs[0] += 1
 
