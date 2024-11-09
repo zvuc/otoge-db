2ebec24f4015264312976c1923b58dedfda8c318
Hansol Kim <kimatg@gmail.com>
2024-11-09 14:51:46 +0900

    feat(script/maimai): support parsing from legacy sgimera data format

--- scripts/update-const.py
+++ scripts/update-const.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
         '--id': {'default':0, 'help':'Song ID, single (2064) or range (802-2310)'},
         '--all': {'action':"store_true", 'help':'Run for all items'},
         '--overwrite': {'action':"store_true", 'help':'Overwrite existing values'},
-        '--clear_cache': {'action':"store_true", 'help':'Clears local cache on run'}
+        '--clear_cache': {'action':"store_true", 'help':'Clears local cache on run'},
+        '--legacy': {'action':"store_true", 'help':'Match with legacy sgimera format'}
     }
     set_args_and_game_module(custom_args)
     main()
